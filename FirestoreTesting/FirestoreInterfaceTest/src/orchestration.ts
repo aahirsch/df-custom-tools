095db91905a1f36efd59efead38f9575cc5e0f84
christopher Sebastian <christophersebastian@vagelos-ve703-00911.apn.wlan.private.upenn.edu>
2022-11-02 21:23:53 -0400

    penultame update, just have to decide on exactly what we want to, how much we want to do it, and time

--- FirestoreTesting/FirestoreInterfaceTest/src/orchestration.ts
+++ FirestoreTesting/FirestoreInterfaceTest/src/orchestration.ts
@@ -8,6 +8,9 @@ import {parser} from "./parser"
 import {conversion} from "./conversion"
 
 import StructureA from "./StructureA"
+import StructureB from "./StructureA"
+import StructureC from "./StructureA"
+import StructureD from "./StructureA"
 
 
 
@@ -16,7 +19,7 @@ declare var require: any
 const admin = require('firebase-admin')
 
 let credentialsLocation = "/Users/christophersebastian/Downloads/heartschat-prod-a505-firebase-adminsdk-dgjo6-35494c7d54.json"
-//../heartschat-prod-creds.json
+
 let credentials = require(credentialsLocation)
 
 admin.initializeApp({
@@ -25,45 +28,70 @@ admin.initializeApp({
 
 const db:Firestore=admin.firestore()
 
+// function wrappings for easy testing
+
+async function massUpload(structure: DatabaseInterface, topLevelCollection: string, dataMessages: any[]){
+  const testingCollection = db.collection(topLevelCollection);
+  for (let i = 0; i<dataMessages.length; i++){
+    await structure.insertMessage(testingCollection, data[i] as Message);
+  }
+}
+
+async function massConvoUpload(structure: DatabaseInterface, topLevelCollection: string, dataMessages: any[]){
+  const testingCollection = db.collection(topLevelCollection);
+  let convos = conversion(dataMessages);
+  for (let i = 0; i<convos.length; i++){
+    await structure.insertConversation(testingCollection,convos[i]);
+  }
+}
+
+async function retriveConvo(structure: DatabaseInterface, topLevelCollection: string, sID: string, aID: string, rID: string){
+  const testingCollection = db.collection(topLevelCollection);
+  await structure.retrieveConversation(testingCollection,sID,aID,rID);
+}
+
+async function retriveSurvey(structure: DatabaseInterface, topLevelCollection: string, sID: string){
+  const testingCollection = db.collection(topLevelCollection);
+  await structure.retrieveSurvey(testingCollection,sID);
+}
+
+async function retAll(structure: DatabaseInterface, topLevelCollection: string){
+  const testingCollection = db.collection(topLevelCollection);
+  await structure.retrieveAll(testingCollection);
+}
+
+async function giveAc2Survey(structure: DatabaseInterface, topLevelCollection: string, reID: string, sIDs: string[]){
+  const testingCollection = db.collection(topLevelCollection);
+  await structure.giveAccessToSurveys(testingCollection, reID, sIDs);
+}
+
+async function getAcSurveys(structure: DatabaseInterface, topLevelCollection: string, reID: string){
+  const testingCollection = db.collection(topLevelCollection);
+  await structure.getAccessibleSurveys(testingCollection, reID);
+}
+
+async function getConvoBetween(structure: DatabaseInterface, topLevelCollection: string, start: Date, end: Date){
+  const testingCollection = db.collection(topLevelCollection);
+  await structure.getConversationsBetween(testingCollection, start, end);
+}
+
+
 
 // get data
 let data  = parser('/Users/christophersebastian/Downloads/negbot_testing_q_conv_merge_2022-06-24.xlsx - _merge (1).csv')
-let snipit = data[0] ;
-let snipit2 = data[0] as Message;
-let endsnipit = data[10];
-// test away
-const testingCollection = db.collection("testing-2")
-
-// have to make csv --> JSON into csv --> Message i think
-async function testUploadMessage(structure: DatabaseInterface,topLevelCollection: string) {
-  const testingCollection = db.collection(topLevelCollection)
-  await structure.insertMessage(testingCollection, data[0] as Message)
-}
-
-testUploadMessage(StructureA,"testing-A")
-
-
-// for(let i = 0; i < data.length; i++){
-//   StructureA.insertMessage(testingCollection, data[i] as Message)
-// }
-
-
-// let conversations = conversion(data);
-// for (let i = 0; i < conversations.length; i++){
-//   StructureA.insertConversation(testingCollection, conversations[i])
-// }
 
 
 
-// StructureA.retrieveConversation(testingCollection,snipit.surveyId, snipit.agentId ,snipit.output) 
+// call tests
+massUpload(StructureA,"testing-A",data);
+massConvoUpload(StructureA,"testing-A",data);
+retriveConvo(StructureA,"testing-A",data[0].surveyId,data[0].agentId,data[0].responseId);
+retriveSurvey(StructureA,"testing-A",data[0].surveyId);
+retAll(StructureA,"testing-A");
+giveAc2Survey(StructureA,"testing-A","Zuckerberg",[data[0].surveyId]);
+getAcSurveys(StructureA,"testing-A", "Zuckerberg");
+getConvoBetween(StructureA,"testing-A", data[0].timestamp,data[50].timestamp);
 
-// StructureA.retrieveSurvey(testingCollection,snipit[0]) 
 
-// StructureA.retrieveAll(testingCollection)
 
-// StructureA.giveAccessToSurveys(testingCollection, "Mark", [snipit.surveId, endsnipit.SurveyId]) 
-
-// StructureA.getAccessibleSurveys(testingCollection, "Mark") 
-
-// StructureA.getConversationsBetween(testingCollection, snipit.timestamp, endsnipit.timestamp) 
 
